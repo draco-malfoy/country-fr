7b2442fd7c72876a2cc3b456cdf6543a3656110d
draco-malfoy <46344490+draco-malfoy@users.noreply.github.com>
2021-05-04 12:52:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/country-fe'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/country-fe/index.html'));
+res.sendFile(path.join(__dirname+'/dist/country-fe/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
